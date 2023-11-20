5fb1de71526f6748132d7520dca1fe3907a1eb4a
DiegoLoorenzo <diegolorenzomendez@gmail.com>
2023-11-20 11:28:21 -0600

    Se modificó el archivo Screen.dart

--- kobe_flutter/lib/Screen.dart
+++ kobe_flutter/lib/Screen.dart
@@ -1,56 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:kobe_flutter/login/LoginPage.dart';
 
-class Screen extends StatefulWidget{
-
+class Screen extends StatefulWidget {
   @override
-  State createState(){
+  State createState() {
     return _ScreenState();
   }
 }
 
-class _ScreenState extends State<Screen>{
+class _ScreenState extends State<Screen> {
   @override
   void initState() {
     super.initState();
   }
-//todo Estilo y diseño del body y boton llamado por clases
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:
+          Color(0xFFFFD974), // Color de fondo para toda la pantalla
       body: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
-            padding:const EdgeInsets.all(16.0),
-            child:Image.asset('assets/icon300.png')
-          /*Image.network(
-            'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiWTnDrG8iUiaNeji8MVMvDuYXQjMveb_ZFPVVJ6DjO4uj0ww1Gmq0aF_S2P-DuDSyjYC5rA7ss3eds92jzQg3hFb7prcn_4w06diBWEnEPzm6sOh_wMsPU1n7U-VkIHoytE9EAHhKoJnv_JG2SkE0_wIgrE14nFoHwSBmfZVofzh4A5q4U9AgbmnbgH8A/s1600/icon250.jpg'
-            ),*/),
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('assets/icon300.png'),
+            /*Image.network(
+                'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiWTnDrG8iUiaNeji8MVMvDuYXQjMveb_ZFPVVJ6DjO4uj0ww1Gmq0aF_S2P-DuDSyjYC5rA7ss3eds92jzQg3hFb7prcn_4w06diBWEnEPzm6sOh_wMsPU1n7U-VkIHoytE9EAHhKoJnv_JG2SkE0_wIgrE14nFoHwSBmfZVofzh4A5q4U9AgbmnbgH8A/s1600/icon250.jpg'
+            ),*/
+          ),
           Padding(
             padding: const EdgeInsets.all(80.0),
-            child: Text("Bienvenido a K.O.B.E", style: TextStyle(color: Colors.black,
-              fontSize: 27,fontFamily: 'Inter',fontWeight: FontWeight.w500,height: 0),),
+            child: Text(
+              "Bienvenido a K.O.B.E",
+              style: TextStyle(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                fontSize: 45,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w500,
+                height: 0,
+              ),
             ),
+          ),
+          SizedBox(height: 20), // Agregado un espacio entre el texto y el botón
           butonScreen(),
         ],
       ),
     );
   }
 
-///todo Boton del Screen
-Widget butonScreen() {
-  return FractionallySizedBox(
-    widthFactor: 0.6,
-    child: ElevatedButton(
-    onPressed: (){
-      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginPage()),
-        (Route<dynamic> route) => false);
-    },
-    child: Text("Empezar", style: TextStyle(color: Color.fromARGB(255, 255, 255, 255),
-      fontSize: 24, fontFamily: 'Inter',fontWeight: FontWeight.w500),),
-    ),
+  Widget butonScreen() {
+    return FractionallySizedBox(
+      widthFactor: 0.8, // Haciendo el botón más ancho
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => LoginPage()),
+            (Route<dynamic> route) => false,
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          primary: Color(0xFFEE7D33), // Cambiando el color del botón
+          padding: EdgeInsets.symmetric(vertical: 15),
+        ),
+        child: Text(
+          "Empezar",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
     );
-}
+  }
 }
--- kobe_flutter/lib/main.dart
+++ kobe_flutter/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-//import 'package:kobe_flutter/login/LoginPage.dart';
-import 'package:kobe_flutter/Screen.dart';
+import 'package:kobe_flutter/login/LoginPage.dart';
+//import 'package:kobe_flutter/Screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +28,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Aplicacion K.O.B.E",
-      home: Screen(),
+      home: LoginPage(),
     );
   }
 }
