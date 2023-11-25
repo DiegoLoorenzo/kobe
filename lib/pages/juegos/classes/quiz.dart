0fedcb7952d8698020a837ead44091d84a4c7e13
Otoniel <abdiasmaldonado.al@laselva.edu.mx>
2023-11-25 17:31:49 -0600

    Avance modulo juegos, parte de la funcionalidad lista, falta diseño y corregir las preguntas.
    Signed-off-by: Otoniel <abdiasmaldonado.al@laselva.edu.mx>

---
+++ lib/pages/juegos/classes/quiz.dart
@@ -0,0 +1,11 @@
+import 'package:kobe_flutter/pages/juegos/classes/question.dart';
+
+class Quiz {
+  String name;
+  List<Question> questions;
+  int right = 0;
+
+  Quiz({required this.name, required this.questions});
+
+  double get percent => (right / questions.length) * 100;
+}
--- lib/pages/juegos/juegos.dart
+++ lib/pages/juegos/juegos.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kobe_flutter/pages/juegos/pages/quiz_page.dart';
+import 'package:kobe_flutter/pages/juegos/pages/elegir_tema.dart';
+import 'package:kobe_flutter/pages/juegos/pages/review_quiz_page.dart';
 
 class juegos extends StatefulWidget {
 
@@ -12,13 +15,48 @@ class _juegos extends State<juegos>{
 
     @override
     Widget build(BuildContext context) {
-      return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(' Esto es juegos')
-            ],
+      return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            textTheme: TextTheme(
+              headline1: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                shadows: [
+                  Shadow(
+                    color: Colors.purple.shade50.withOpacity(.3),
+                    offset: const Offset(3, 3),
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              headline2: TextStyle(
+                color: Colors.purple.shade50,
+                fontSize: 20,
+              ),
+              bodyText1: TextStyle(
+                color: Colors.purple.shade50,
+                fontSize: 18,
+              ),
+            ),
+            cardTheme: CardTheme(
+              elevation: 6,
+              color: Colors.indigo.shade900,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
           ),
-        );
+          initialRoute: '/',
+          routes: {
+            '/': (context) => const elegir_tema(),
+            '/quiz-page': (context) => const QuizPage(),
+            //'/review-page': (context) => const ReviewQuizPage(),
+          });;
+
     }
+
 }
