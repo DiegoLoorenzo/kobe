f8e8c07108a1b34e610ee6169dd1189d47b2e2f9
Maribel Gomez <maribeldg0202@gmail.com>
2023-12-03 11:18:59 -0600

    Diseño del  modulo de recetas

---
+++ lib/pages/Recetas/RecetaComplementaria.dart
@@ -0,0 +1,340 @@
+import 'package:flutter/material.dart';
+
+class RecetaComplementaria extends StatefulWidget {
+  const RecetaComplementaria({super.key});
+
+  @override
+  State<RecetaComplementaria> createState() => _RecetaComplementariaState();
+}
+
+class _RecetaComplementariaState extends State<RecetaComplementaria> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.medium(
+            backgroundColor: Color(0xFFFFD974),
+            leading: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(Icons.arrow_back),
+            ),
+            automaticallyImplyLeading: true,
+            title: const Text('Receta Complementaria'),
+            centerTitle: true,
+          ),
+
+          //Container 1
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 915,
+                  color: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  child: Column(
+                    children: [
+                      // Image.asset(
+                      //   'assets/img.jpg',
+                      //   width: 200,
+                      //   height: 200,
+                      // ),
+                      Image.network(
+                        'https://images.ctfassets.net/awb1we50v0om/2Spf80TME2zIhLqsi3Zxv9/919421a45f3260ee426c99c35235f1c8/Plates03__3__copy3.jpg',
+                        width: 200,
+                        height: 200,
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        controller: null,
+                        decoration: const InputDecoration(
+                          labelText: 'Nombre de la receta',
+                          prefixIcon: Icon(Icons.sticky_note_2_outlined),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Tamaño de porción',
+                          prefixIcon: Icon(Icons.add_box_outlined),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Numero de porción',
+                          prefixIcon: Icon(Icons.numbers_rounded),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Container(
+                        height: 20,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.orange),
+                        child: const Text(
+                          'Tiempo de preparación',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                              child: Container(
+                                  //color: Colors.green,
+                                  decoration: BoxDecoration(
+                                      color: Colors.transparent,
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: TextFormField(
+                                    textAlign: TextAlign.center,
+                                    maxLength: 2,
+                                    keyboardType: TextInputType.number,
+                                    decoration: const InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        hintText: '00',
+                                        counterText: ''),
+                                  ))),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(':'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.transparent,
+                                    borderRadius: BorderRadius.circular(12)),
+                                child: TextFormField(
+                                  textAlign: TextAlign.center,
+                                  maxLength: 2,
+                                  keyboardType: TextInputType.number,
+                                  decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      hintText: '00',
+                                      counterText: ''),
+                                )),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(':'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.circular(12)),
+                              child: TextFormField(
+                                textAlign: TextAlign.center,
+                                maxLength: 2,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: '00',
+                                    counterText: ''),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10.0),
+                      Container(
+                        height: 20,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.orange),
+                        child: const Text(
+                          'Tiempo de cocción',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                              child: Container(
+                                  //color: Colors.green,
+                                  decoration: BoxDecoration(
+                                      color: Colors.transparent,
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: TextFormField(
+                                    textAlign: TextAlign.center,
+                                    maxLength: 2,
+                                    keyboardType: TextInputType.number,
+                                    decoration: const InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        hintText: '00',
+                                        counterText: ''),
+                                  ))),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(':'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.transparent,
+                                    borderRadius: BorderRadius.circular(12)),
+                                child: TextFormField(
+                                  textAlign: TextAlign.center,
+                                  maxLength: 2,
+                                  keyboardType: TextInputType.number,
+                                  decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      hintText: '00',
+                                      counterText: ''),
+                                )),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(':'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.circular(12)),
+                              child: TextFormField(
+                                textAlign: TextAlign.center,
+                                maxLength: 2,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: '00',
+                                    counterText: ''),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Temperatura de cocción',
+                          prefixIcon: Icon(Icons.thermostat_sharp),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Temperatura de servicio',
+                          prefixIcon: Icon(Icons.thermostat),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Tiempo de conservación',
+                          prefixIcon: Icon(Icons.timer),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Temperatura de conservación',
+                          prefixIcon: Icon(Icons.thermostat_sharp),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //Container 2
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 370,
+                  color: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: null,
+                        decoration: const InputDecoration(
+                          labelText: 'Clasificación',
+                          prefixIcon: Icon(Icons.class_outlined),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Tipos de corte',
+                          prefixIcon: Icon(Icons.soup_kitchen_outlined),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Alianza con bebida',
+                          prefixIcon: Icon(Icons.assignment_outlined),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Métodos de cocción',
+                          prefixIcon: Icon(Icons.soup_kitchen_outlined),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Tipo de emplatado',
+                          prefixIcon: Icon(Icons.timer),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
